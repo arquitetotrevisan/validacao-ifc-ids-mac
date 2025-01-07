79ed25f280a63e30b11b3f7afa268f34692c7480
Ricardo Trevisan <98924405+arquitetotrevisan@users.noreply.github.com>
2025-01-07 15:12:20 -0300

    Update validate_ifc_with_ids.py

--- validate_ifc_with_ids.py
+++ validate_ifc_with_ids.py
@@ -118,7 +118,7 @@ def main():
                         if key == "IfcPostalAddress":
                             txt_file.write(f"  Endereço: {value}\n")
                         else:
-                                txt_file.write(f"  {key}: {value}\n")
+                            txt_file.write(f"  {key}: {value}\n")
 
 
     # Salva o relatório CSV
